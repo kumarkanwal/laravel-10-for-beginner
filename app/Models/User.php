dc5c8963fe058c0c248189a255752baaba86f815
kanwal kumar <kanwalkumarqalandri8272@gmail.com>
2024-03-31 20:24:13 +0500

    commit

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -32,6 +34,7 @@ class User extends Authenticatable
         'remember_token',
     ];
 
+
     /**
      * Get the attributes that should be cast.
      *
@@ -44,4 +47,7 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,14 +1,53 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\DB;
+
+// Route::get('/', function () {
+//     return view('dashboard');
+// });
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    // return view('dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
+    // $users = DB::select("select * from users ");
+    // $users = DB::insert("insert into users (username, email,password) values(?,?,?)" , ["vishal","vishal@gmail.com","vishalishere"]);
+    // $users = DB::update("update users set email = ? where id = ?", ["vishal9034@gmail.com", 2]);
+    // $users = DB::delete("delete from users where id = ?",[2]);
+
+    // $users = DB::table('users')->get();
+    // $users = DB::table('users')->where("id", 1)->get();
+    // $users = DB::table('users')->insert([
+    //     'username' => 'vishal',
+    //     'email' => 'vishal903@gmail.com',
+    //     'password' => 'velo903'
+    //     ]);
+    // $users = DB::table('users')->where('id', 1)->update(['email' => 'kanwal1234@gmail.com']);
+    // $users = DB::table('users')->where('id', 1)->delete();
+        
+
+
+    
+// $user = User::all();
+    // $user->delete();
+    //  $user->update(
+    //     ['email' => 'kanwal123465@gmail.com', ]
+    // );
+    //    $user = User::create([
+    //     'name' => 'vvishal',
+    //     'email' => 'vvishal903@gmail.com',
+    //     'password' => 'vvelo903'
+    //    ]); 
+    $user = User::create([
+        'name' => 'Sarthak',
+        'email'=> 'sarthak4@gmail.com',
+        'password'=> bcrypt('kanwal1123')
+    ]);
+    dd($user);
+
+   
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
@@ -18,3 +57,4 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
